47580fc509ca1c4ad2257fd2734b8aa7166bdcab
Nikoleta <t.nikoleta@kt-computer.de>
2021-12-16 14:38:39 +0100

    Recording implemented

--- app/src/main/java/com/example/whatdidisay/RecordingActivity.java
+++ app/src/main/java/com/example/whatdidisay/RecordingActivity.java
@@ -1,108 +1,154 @@
 package com.example.whatdidisay;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.speech.RecognitionListener;
 import android.speech.RecognizerIntent;
 import android.speech.SpeechRecognizer;
-import android.speech.tts.TextToSpeech;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
-import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class RecordingActivity extends AppCompatActivity implements RecognitionListener {
+public class RecordingActivity extends AppCompatActivity {
 
-    private void log(String msg) {
-        Log.d("MainActivity", msg);
-    }
-    private static final String TAG = "MySttActivity";
-
-    TextView mText;
-    private SpeechRecognizer sr;
-    private Intent recognizerIntent;
-    private String speechResults;
+    private boolean isRecordingActive = false; // false:= recording has not started, true := currently recording
+    private String text = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recording_activity);
 
-        mText = (TextView) findViewById(R.id.text_box);
+        checkPermission(); // Checking Permission on Runtime. Important for Android Marshmallow or later
 
-        if (!SpeechRecognizer.isRecognitionAvailable(this)) {
-            log("no speech recognizer available");
-            finish();
-            return;
+        EditText displayText = findViewById(R.id.text_box);
+
+        final SpeechRecognizer mSpeechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+
+        final Intent mSpeechRecognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        mSpeechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "de-DE");
+
+
+        mSpeechRecognizer.setRecognitionListener(new RecognitionListener() {
+            @Override
+            public void onReadyForSpeech(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onBeginningOfSpeech() {
+
+            }
+
+            @Override
+            public void onRmsChanged(float v) {
+
+            }
+
+            @Override
+            public void onBufferReceived(byte[] bytes) {
+
+            }
+
+            @Override
+            public void onEndOfSpeech() {
+
+            }
+
+            @Override
+            public void onError(int i) {
+
+            }
+
+            @Override
+            public void onResults(Bundle bundle) {
+                //getting all the matches
+                ArrayList<String> matches = bundle.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+
+                if (matches != null) {
+                    displayText.setText(matches.get(0));
+                    int i = 0;
+                    for (String match : matches) {
+                        //text = new StringBuilder().append(text).append(" ").append(match).toString();
+                        displayText.setText(match);
+                        i++;
+                    }
+                }
+            }
+
+            @Override
+            public void onPartialResults(Bundle bundle) {
+
+            }
+
+            @Override
+            public void onEvent(int i, Bundle bundle) {
+
+            }
+        });
+
+
+        Button recordButton = (Button) findViewById(R.id.record_button);
+        recordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!isRecordingActive) { // recording is not active
+                    isRecordingActive = true;
+                    displayText.setHint("Listening...");
+                    mSpeechRecognizer.startListening(mSpeechRecognizerIntent);
+                } else { // recording is active
+                    isRecordingActive = false;
+                    mSpeechRecognizer.stopListening();
+                    // ToDo write text in the database
+                    //text = "";
+                    displayText.setHint("Ready to start recording.");
+                }
+            }
+        });
+
+
+        Button backButton = (Button) findViewById(R.id.back_button);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+    private void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!(ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED)) {
+                Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS,
+                        Uri.parse("package:" + getPackageName()));
+                startActivity(intent);
+                finish();
+            }
         }
-
-        sr = SpeechRecognizer.createSpeechRecognizer(this);
-        sr.setRecognitionListener(this);
-
-        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "de-DE");
-
-        sr.startListening(recognizerIntent);
-
-
-
-    }
-
-
-    @Override
-    public void onReadyForSpeech(Bundle bundle) {
-
     }
 
     @Override
-    public void onBeginningOfSpeech() {
-
+    public void onResume() {
+        super.onResume();
+        //Set the date label
+        String currentDate = getIntent().getStringExtra("date");
+        TextView dateText = (TextView) findViewById(R.id.transcript_text);
+        dateText.setText(currentDate);
     }
 
-    @Override
-    public void onRmsChanged(float v) {
-
-    }
-
-    @Override
-    public void onBufferReceived(byte[] bytes) {
-
-    }
-
-    @Override
-    public void onEndOfSpeech() {
-        sr.stopListening();
-    }
-
-    @Override
-    public void onError(int i) {
-        sr.startListening(recognizerIntent);
-    }
-
-    @Override
-    public void onResults(Bundle results) {
-        String str = new String();
-        Log.d(TAG, "onResults " + results);
-        ArrayList data = results.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
-        for (int i = 0; i < data.size(); i++)
-        {
-            Log.d(TAG, "result " + data.get(i));
-            str += data.get(i);
-        }
-        mText.setText("results: "+String.valueOf(data.size()));
-    }
-
-    @Override
-    public void onPartialResults(Bundle bundle) {
-
-    }
-
-    @Override
-    public void onEvent(int i, Bundle bundle) {
-
-    }
 }
